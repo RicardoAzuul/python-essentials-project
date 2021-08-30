3d53016df7a9f94862f5b8f92bfecd41c66ba544
Richard Blaauw <r.t.blaauw@live.com>
2021-08-30 19:06:51 +0000

    Return dictionary of how much to make of each sandwich for the next market day

--- run.py
+++ run.py
@@ -134,8 +134,23 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-
+    return stock_data
+    
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
 
+
+def get_stock_values(data):
+    """
+    Get stock values of sandwiches to be made for next day
+    """
+    headings = []
+    sheet = SHEET.worksheet("stock")
+    headings = sheet.row_values(1)
+    dictionary = dict(zip(headings, data))
+    return dictionary
+
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
